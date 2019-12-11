bc457f119821493e3276fd2505d5b8b5c3df8f27
CaptTrews <capttrews@gmail.com>
2019-12-11 00:31:33 +0000

    Updated from network content collector
    Signed-off-by: CaptTrews <capttrews@gmail.com>

--- plugins/modules/nxos_user.py
+++ plugins/modules/nxos_user.py
@@ -378,15 +378,7 @@ def main():
         supports_check_mode=True,
     )
 
-    warnings = list()
-    if module.params["password"] and not module.params["configured_password"]:
-        warnings.append(
-            'The "password" argument is used to authenticate the current connection. '
-            + 'To set a user password use "configured_password" instead.'
-        )
-
     result = {"changed": False}
-    result["warnings"] = warnings
 
     want = map_params_to_obj(module)
     have = map_config_to_obj(module)
