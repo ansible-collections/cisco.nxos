0ecfe9a6f002a0d7592affaddc2a9673a5e183d4
NilashishC <nilashishchakraborty8@gmail.com>
2020-04-29 22:29:11 +0530

    Add new states for nxos_lag_interfaces
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- plugins/module_utils/network/nxos/config/lag_interfaces/lag_interfaces.py
+++ plugins/module_utils/network/nxos/config/lag_interfaces/lag_interfaces.py
@@ -4,7 +4,7 @@
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 """
-The junos_lag_interfaces class
+The nxos_lag_interfaces class
 It is in this file where the current configuration (as dict)
 is compared to the provided configuration (as dict) and the command set
 necessary to bring the current configuration to it's desired end-state is
@@ -43,14 +43,14 @@ class Lag_interfaces(ConfigBase):
     def __init__(self, module):
         super(Lag_interfaces, self).__init__(module)
 
-    def get_lag_interfaces_facts(self):
+    def get_lag_interfaces_facts(self, data=None):
         """ Get the 'facts' (the current configuration)
 
         :rtype: A dictionary
         :returns: The current configuration as a dictionary
         """
         facts, _warnings = Facts(self._module).get_facts(
-            self.gather_subset, self.gather_network_resources
+            self.gather_subset, self.gather_network_resources, data=data
         )
         lag_interfaces_facts = facts["ansible_network_resources"].get(
             "lag_interfaces"
@@ -69,9 +69,15 @@ class Lag_interfaces(ConfigBase):
         commands = list()
         warnings = list()
 
-        existing_lag_interfaces_facts = self.get_lag_interfaces_facts()
-        commands.extend(self.set_config(existing_lag_interfaces_facts))
-        if commands:
+        if self.state in self.ACTION_STATES:
+            existing_lag_interfaces_facts = self.get_lag_interfaces_facts()
+        else:
+            existing_lag_interfaces_facts = []
+
+        if self.state in self.ACTION_STATES or self.state == "rendered":
+            commands.extend(self.set_config(existing_lag_interfaces_facts))
+
+        if commands and self.state in self.ACTION_STATES:
             if not self._module.check_mode:
                 resp = self._connection.edit_config(commands)
                 if "response" in resp:
@@ -81,13 +87,33 @@ class Lag_interfaces(ConfigBase):
                             if err_str.lower().startswith("cannot add"):
                                 self._module.fail_json(msg=err_str)
             result["changed"] = True
-        result["commands"] = commands
 
-        changed_lag_interfaces_facts = self.get_lag_interfaces_facts()
+        if self.state in self.ACTION_STATES:
+            result["commands"] = commands
 
-        result["before"] = existing_lag_interfaces_facts
-        if result["changed"]:
-            result["after"] = changed_lag_interfaces_facts
+        if self.state in self.ACTION_STATES or self.state == "gathered":
+            changed_lag_interfaces_facts = self.get_lag_interfaces_facts()
+
+        elif self.state == "rendered":
+            result["rendered"] = commands
+
+        elif self.state == "parsed":
+            running_config = self._module.params["running_config"]
+            if not running_config:
+                self._module.fail_json(
+                    msg="value of running_config parameter must not be empty for state parsed"
+                )
+            result["parsed"] = self.get_lag_interfaces_facts(
+                data=running_config
+            )
+
+        if self.state in self.ACTION_STATES:
+            result["before"] = existing_lag_interfaces_facts
+            if result["changed"]:
+                result["after"] = changed_lag_interfaces_facts
+
+        elif self.state == "gathered":
+            result["gathered"] = changed_lag_interfaces_facts
 
         result["warnings"] = warnings
         return result
@@ -123,6 +149,15 @@ class Lag_interfaces(ConfigBase):
                   to the desired configuration
         """
         state = self._module.params["state"]
+        if (
+            state in ("overridden", "merged", "replaced", "rendered")
+            and not want
+        ):
+            self._module.fail_json(
+                msg="value of config parameter must not be empty for state {0}".format(
+                    state
+                )
+            )
         commands = list()
 
         if state == "overridden":
@@ -131,7 +166,7 @@ class Lag_interfaces(ConfigBase):
             commands.extend(self._state_deleted(want, have))
         else:
             for w in want:
-                if state == "merged":
+                if state in ["merged", "rendered"]:
                     commands.extend(self._state_merged(w, have))
                 if state == "replaced":
                     commands.extend(self._state_replaced(w, have))
@@ -279,7 +314,7 @@ class Lag_interfaces(ConfigBase):
         commands = []
         if not obj_in_have:
             return commands
-        for m in obj_in_have["members"]:
+        for m in obj_in_have.get("members", []):
             commands.append("interface" + " " + m["member"])
             commands.append("no channel-group")
         return commands
