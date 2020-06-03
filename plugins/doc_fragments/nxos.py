a847c61e991ab5951fc1d2720a2c9aba63c9a863
Nathaniel Case <Qalthos@gmail.com>
2020-06-03 10:52:15 -0400

    Woah, we really should not be promoting provider for NX-API (#77)
    Woah, we really should not be promoting provider for NX-API
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/doc_fragments/nxos.py
+++ plugins/doc_fragments/nxos.py
@@ -12,7 +12,8 @@ class ModuleDocFragment(object):
     description:
     - B(Deprecated)
     - 'Starting with Ansible 2.5 we recommend using C(connection: network_cli).'
-    - This option is only required if you are using NX-API.
+    - 'Starting with Ansible 2.6 we recommend using C(connection: httpapi) for NX-API.'
+    - This option will be removed in a release after 2022-06-01.
     - For more information please see the L(NXOS Platform Options guide, ../network/user_guide/platform_nxos.html).
     - HORIZONTALLINE
     - A dict object containing connection details.
