d3a4e68f5e80569e019001af4d8c016711f63ef8
NilashishC <nilashishchakraborty8@gmail.com>
2020-09-30 17:42:43 +0530

    Fix doc
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- plugins/modules/nxos_ospfv3.py
+++ plugins/modules/nxos_ospfv3.py
@@ -45,7 +45,7 @@ options:
     description:
     - This option is used only with state I(parsed).
     - The value of this option should be the output received from the NX-OS device
-      by executing the command B(show running-config | section "^router ospf .*").
+      by executing the command B(show running-config | section "^router ospfv3").
     - The state I(parsed) reads the configuration from C(running_config) option and
       transforms it into Ansible structured data as per the resource module's argspec
       and the value is then returned in the I(parsed) key within the result.
