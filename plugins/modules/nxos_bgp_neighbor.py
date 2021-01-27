54c271b1e1e81fe5126cf6bbb0172b1d30be3c77
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2021-01-28 04:04:30 +0530

    Deprecate BGP modules (#239)
    Deprecate legacy BGP modules
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- plugins/modules/nxos_bgp_neighbor.py
+++ plugins/modules/nxos_bgp_neighbor.py
@@ -24,11 +24,15 @@ DOCUMENTATION = """
 module: nxos_bgp_neighbor
 extends_documentation_fragment:
 - cisco.nxos.nxos
-short_description: Manages BGP neighbors configurations.
+short_description: (deprecated, removed after 2023-01-27) Manages BGP neighbors configurations.
 description:
 - Manages BGP neighbors configurations on NX-OS switches.
 version_added: 1.0.0
 author: Gabriele Gerbino (@GGabriele)
+deprecated:
+  alternative: nxos_bgp_global
+  why: Updated module released with more functionality.
+  removed_at_date: '2023-01-27'
 notes:
 - Tested against NXOSv 7.3.(0)D1(1) on VIRL
 - C(state=absent) removes the whole BGP neighbor configuration.
