4f7186e95ec6ddcf94665a760a41c2354a320a47
Nilashish Chakraborty <nilashishchakraborty8@gmail.com>
2021-02-20 14:34:06 +0530

    Update unit test imports (#251)
    Update unit test imports
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- tests/unit/modules/network/nxos/test_nxos_ospfv2.py
+++ tests/unit/modules/network/nxos/test_nxos_ospfv2.py
@@ -41,7 +41,7 @@ class TestNxosOspfv2Module(TestNxosModule):
         super(TestNxosOspfv2Module, self).setUp()
 
         self.mock_get_resource_connection = patch(
-            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.resource_module.get_resource_connection"
+            "ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module_base.get_resource_connection"
         )
         self.get_resource_connection = (
             self.mock_get_resource_connection.start()
