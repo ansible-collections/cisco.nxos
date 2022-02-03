26abd710caed8472c2ca317dedcdf6c7952d0d1a
KB-perByte <sagpaul@redhat.com>
2022-02-03 15:42:37 +0530

    updated reference of base plugin to netcommon

--- plugins/netconf/nxos.py
+++ plugins/netconf/nxos.py
@@ -38,6 +38,9 @@ options:
 """
 
 from ansible.plugins.netconf import NetconfBase
+from ansible_collections.ansible.netcommon.plugins.plugin_utils.netconf_base import (
+    NetconfBase,
+)
 
 
 class Netconf(NetconfBase):
