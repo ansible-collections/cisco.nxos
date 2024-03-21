b6992cb3968fa6d5e7d24307acd25ce20d4a6369
Ashwini Mhatre <amhatre@amhatre-thinkpadt14sgen2i.pnq.csb>
2024-03-21 13:59:29 +0530

    test integretion plugin

--- plugins/modules/nxos_bgp_global.py
+++ plugins/modules/nxos_bgp_global.py
@@ -3,7 +3,7 @@
 # Copyright 2021 Red Hat
 # GNU General Public License v3.0+
 # (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-
+# test integretion
 """
 The module file for nxos_bgp_global
 """
--- tests/integration/targets/nxos_config/templates/config.js
+++
@@ -1,3 +0,0 @@
-interface Ethernet2/5
-   description test description from ansible
-   shutdown
