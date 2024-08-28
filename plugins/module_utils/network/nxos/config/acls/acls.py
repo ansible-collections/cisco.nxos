abed08a8165d5f3647e1075efe37693a5894357a
Sean Sullivan <ssulliva@redhat.com>
2024-08-28 04:56:18 -0400

    Fixes Resource module cisco.nxos.nxos_acls is not correctly converting the ports under port_protocol range keys (#888)
    * bugfix/acl port protocol lookup for range start and end
    
    * Create bugfix_vrf_range_resolution.yml
    
    Create Changelog Fragment
    
    * chore: auto fixes from pre-commit.com hooks
    
    * updates
    
    * chore: auto fixes from pre-commit.com hooks
    
    * add unit test case
    
    * chore: auto fixes from pre-commit.com hooks
    
    * update test
    
    * allow non-numeric values to pass through as strings
    
    * chore: auto fixes from pre-commit.com hooks
    
    * allow non-numeric values to pass through as strings
    
    * chore: auto fixes from pre-commit.com hooks
    
    * fix test
    
    * chore: auto fixes from pre-commit.com hooks
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Sagar Paul <sagpaul@redhat.com>
    Co-authored-by: Ruchi Pakhle <ruchipakhle@gmail.com>

--- plugins/module_utils/network/nxos/config/acls/acls.py
+++ plugins/module_utils/network/nxos/config/acls/acls.py
@@ -242,17 +242,29 @@ class Acls(ConfigBase):
                                                     int(val)
                                                 ]
                                         else:
-                                            st = int(ace[x]["port_protocol"]["range"]["start"])
-                                            end = int(ace[x]["port_protocol"]["range"]["end"])
+                                            st = ace[x]["port_protocol"]["range"]["start"]
+                                            end = ace[x]["port_protocol"]["range"]["end"]
 
-                                            if st in port_protocol.keys():
-                                                ace[x]["port_protocol"]["range"]["start"] = (
-                                                    port_protocol[st]
-                                                )
-                                            if end in port_protocol.keys():
-                                                ace[x]["port_protocol"]["range"]["end"] = (
-                                                    port_protocol[end]
-                                                )
+                                            if st.isdigit():
+                                                if int(st) in port_protocol.keys():
+                                                    ace[x]["port_protocol"]["range"]["start"] = (
+                                                        port_protocol[int(st)]
+                                                    )
+                                            else:
+                                                if st in port_protocol.keys():
+                                                    ace[x]["port_protocol"]["range"]["start"] = (
+                                                        port_protocol[st]
+                                                    )
+                                            if end.isdigit():
+                                                if int(end) in port_protocol.keys():
+                                                    ace[x]["port_protocol"]["range"]["end"] = (
+                                                        port_protocol[int(end)]
+                                                    )
+                                            else:
+                                                if end in port_protocol.keys():
+                                                    ace[x]["port_protocol"]["range"]["end"] = (
+                                                        port_protocol[end]
+                                                    )
         return want
 
     def set_state(self, want, have):
