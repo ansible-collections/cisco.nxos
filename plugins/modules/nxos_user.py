398212682263b9499122c7fac8840df97bb9c23b
AAYUSH ANAND <aayushanand407@gmail.com>
2024-10-21 19:11:00 +0530

    fixes_nxos_user purge behaviour

--- plugins/modules/nxos_user.py
+++ plugins/modules/nxos_user.py
@@ -411,6 +411,14 @@ def update_objects(want, have):
                     updates.append((entry, item))
     return updates
 
+def get_configured_usernames(module):
+    config_output = run_commands(module, [{"command": "show running-config | section ^username", "output": "text"}])
+    usernames = set()
+    for line in config_output[0].splitlines():
+        if line.startswith("username "):
+            username = line.split()[1]
+            usernames.add(username)
+    return usernames
 
 def main():
     """main entry point for module execution"""
@@ -457,9 +465,14 @@ def main():
     commands = map_obj_to_commands(update_objects(want, have), module)
 
     if module.params["purge"]:
-        want_users = [x["name"] for x in want]
-        have_users = [x["name"] for x in have]
-        for item in set(have_users).difference(want_users):
+        want_users = set([x["name"] for x in want])
+        have_users = set([x["name"] for x in have])
+        
+        configured_usernames = get_configured_usernames(module)
+        
+        non_local_users=have_users.difference(want_users).difference(configured_usernames)
+        
+        for item in configured_usernames.difference(non_local_users):
             if item != "admin":
                 item = item.replace("\\", "\\\\")
                 commands.append("no username %s" % item)
